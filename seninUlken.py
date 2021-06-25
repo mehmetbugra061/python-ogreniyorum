b4008d0331120bb6871d1323276497255151497d
Mehmet Buğra Şişman <86467817+mehmetbugra061@users.noreply.github.com>
2021-06-25 16:29:15 +0300

    g7 ülkelerinin kodlarını ekledim

---
+++ seninUlken.py
@@ -0,0 +1,18 @@
+seninUlken = input("lütfen bir ülke yazınız " )
+
+if(seninUlken == "amerika"):
+    print("yazdığınız ülke G7 ülkeleri arasında yer alıyor")
+elif(seninUlken == "fransa"):
+    print("yazdığınız ülke G7 ülkeleri arasında yer alıyor")
+elif(seninUlken == "almanya"):
+    print("yazdığınız ülke G7 ülkeleri arasında yer alıyor")
+elif(seninUlken == "italya"):
+    print("yazdığınız ülke G7 ülkeleri arasında yer alıyor")
+elif(seninUlken == "ingiltere"):
+    print("yazdığınız ülke G7 ülkeleri arasında yer alıyor")
+elif(seninUlken == "japonya"):
+    print("yazdığınız ülke G7 ülkeleri arasında yer alıyor ")
+elif(seninUlken == "kanada"):
+    print("yazdığınız ülke G7 ülkeleri arasında yer alıyor")
+else:
+    print("yazdığınız ülke G7 ülkeleri arasında yer almıyor ")
