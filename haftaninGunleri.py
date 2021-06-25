c2f7e9ee6d08d48dd6991eb25914e73b0d973179
Mehmet Buğra Şişman <86467817+mehmetbugra061@users.noreply.github.com>
2021-06-25 12:09:38 +0300

    haftanin günleri kodlarını ekledim

---
+++ haftaninGunleri.py
@@ -0,0 +1,18 @@
+haftaninGünleri = int(input())
+
+if(haftaninGünleri == 1):
+    print("pazartesi")
+elif(haftaninGünleri == 2):
+    print("sali")
+elif(haftaninGünleri == 3):
+    print("carsamba")
+elif(haftaninGünleri == 4):
+    print("persembe")
+elif(haftaninGünleri == 5):
+    print("cuma")
+elif(haftaninGünleri == 6):
+    print("cumartesi")
+elif(haftaninGünleri == 7):
+    print("pazar")
+else:
+    print("oyle bir gün bulunmamaktadır")
